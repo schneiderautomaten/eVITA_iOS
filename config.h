c58f353b2ae3eedb982ebbd5d6743e01dc4389b8
Michael Schröter <michael.schroeter@explicatis.com>
2018-02-16 12:28:59 +0100

    Editing frameworks and dependencies so app is running on device in developer-mod

---
+++ config.h
@@ -0,0 +1,8 @@
+//
+//  config.h
+//  Telegraph
+//
+//  Created by Michael Schröter on 15.02.18.
+//
+#define SETUP_API_ID(apiId) apiId = 123423;
+#define SETUP_API_HASH(apiHash) apiHash = @"28a999b8d35e37bc6fc0140cf1b0a22d";
